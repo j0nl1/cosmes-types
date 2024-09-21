3951e4c1fd9b9cb7d68fdaa81abd854954d9c6c2
j0nl1 <jonlitech@gmail.com>
2024-09-21 20:21:47 +0100

    chore: init project config

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "tsup";
+
+/**
+ * @see https://tsup.egoist.dev/#usage
+ */
+export default defineConfig({
+  dts: true,
+  shims: true,
+  bundle: true,
+  outDir: "build",
+  platform: "neutral",
+  target: "esnext",
+  format: ["esm", "cjs"],
+  treeshake: "recommended",
+  entry: ["**/**.ts"],
+});
