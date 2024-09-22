438ee7f2248ec485cfdfefb9bf10e1e780b2407d
Javier Aceña <40375118+j0nl1@users.noreply.github.com>
2024-09-22 17:08:20 +0200

    feat: update syntax for services and buf v2 (#2)

---
+++ plugins/protoc-gen-cosmes.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/env -S npx tsx
+// @ts-check
+
+/**
+ * This is a custom plugin for `buf` that generates TS files from the services
+ * defined in the proto files, and is referred to by the root `buf.gen.yaml`.
+ * Files generated using this plugin contains the `_cosmes` suffix.
+ *
+ * Do not convert this to a TS file as it runs 4x slower!
+ */
+
+import {
+  createEcmaScriptPlugin,
+  runNodeJs,
+  type Schema,
+  type GeneratedFile,
+  safeIdentifier,
+} from "@bufbuild/protoplugin";
+import type { DescService } from "@bufbuild/protobuf";
+
+export function generateTs(schema: Schema) {
+  for (const protoFile of schema.files) {
+    const file = schema.generateFile(protoFile.name + "_cosmes.ts");
+    file.preamble(protoFile);
+    for (const service of protoFile.services) {
+      generateService(file, service);
+    }
+  }
+}
+
+function generateService(f: GeneratedFile, service: DescService) {
+  f.print("const TYPE_NAME = ", f.string(service.typeName), ";");
+  f.print("");
+  for (const method of service.methods) {
+    const inputType = f.importShape(method.input);
+    const inputDesc = f.importSchema(method.input);
+    const outputType = f.importShape(method.output);
+    const outputDesc = f.importSchema(method.output);
+    f.print(f.jsDoc(method));
+    const { toBinary, fromBinary } = f.runtime;
+    f.print(f.export("const", safeIdentifier(service.name + method.name + "Service")), "= {");
+    f.print("  typeName: TYPE_NAME,");
+    f.print("  method: ", f.string(method.name), ",");
+    f.print("  request: {");
+    f.print("      toBinary(msg: Omit<", inputType, ", '$unknown' | '$typeName'>) {");
+    f.print("        return ", toBinary, "(", inputDesc, ", msg as ", inputType, ");");
+    f.print("      },");
+    f.print("      fromBinary(bytes: Uint8Array) {");
+    f.print("        return ", fromBinary, "(", inputDesc, ", bytes);");
+    f.print("      }");
+    f.print("  },");
+    f.print("  response: {");
+    f.print("      toBinary(msg: Omit<", outputType, ", '$unknown' | '$typeName'>) {");
+    f.print("        return ", toBinary, "(", outputDesc, ", msg as ", outputType, ");");
+    f.print("    },");
+    f.print("    fromBinary(bytes: Uint8Array) {");
+    f.print("      return ", fromBinary, "(", outputDesc, ", bytes);");
+    f.print("    }");
+    f.print("  }");
+    f.print("} as const;");
+    f.print("");
+  }
+}
+
+runNodeJs(
+  createEcmaScriptPlugin({
+    name: "protoc-gen-cosmes",
+    version: "v0.0.1",
+    generateTs,
+  }),
+);
